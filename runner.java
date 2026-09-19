3e15cff027b1e636f1901bfd582c8aeb0b5b8b7a
Enrique Polanco Jr. <enriquepolancojr@gmail.com>
2026-09-19 17:55:25 -0500

    Added a for loop

--- runner.java
+++ runner.java
@@ -1,5 +1,8 @@
 public class runner {
     public static void main(String[] args) {
         System.out.println("Hello everyone.");
+        for(int i = 0; i < 5; i++) {
+            System.out.println("This is iteration number: " + i);
+        }
     }
 }
